7ea239b4e47d99d303e7c7530da1f23ad5cbd87d
GEL2345 <G.E.Loake@lse.ac.uk>
2023-09-25 10:59:43 +0100

    Intital Commit Tamagotchi

---
+++ src/Tamagotchi.java
@@ -0,0 +1,11 @@
+public class Tamagotchi {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Tamagotchi(String name) {
+        this.name = name;
+    }
+}
